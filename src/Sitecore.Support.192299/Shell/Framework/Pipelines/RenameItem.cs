69b455a5f9f666e6c7a9cca08b36b993e9dbc7a5
Nikolay Mitikov <nmi@sitecore.net>
2017-11-08 13:40:16 +0800

    #192299: Plug in improved logic

---
+++ src/Sitecore.Support.192299/Shell/Framework/Pipelines/RenameItem.cs
@@ -0,0 +1,32 @@
+﻿namespace Sitecore.Support.Shell.Framework.Pipelines
+{
+  using Sitecore.Diagnostics;
+  using Sitecore.Jobs;
+  using Sitecore.Web.UI.Sheer;
+
+  public class RenameItem : Sitecore.Shell.Framework.Pipelines.RenameItem
+  {
+    public override void RepairLinks(ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      var itemToHaveLinksUpdated = GetItem(args);
+      if (itemToHaveLinksUpdated == null)
+      {
+        base.RepairLinks(args);
+        return;
+      }
+
+      JobOptions options = new JobOptions(
+        jobName: "LinkUpdater",
+        category: "LinkUpdater",
+        siteName: Context.Site.Name,
+        obj: new Support.Links.LinkUpdaterJob(itemToHaveLinksUpdated),
+        methodName: "Update")
+      {
+        ContextUser = Context.User
+      };
+
+      JobManager.Start(options);
+    }
+  }
+}
